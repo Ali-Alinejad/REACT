7620fc9c3ee10891bf1eb90cd0e21269c7ee0538
WINFOORD <winford.op@gmail.com>
2024-08-08 09:07:21 +0330

    set api for geting  flag

--- 8-8 - API-COUNTRIES/scripts.js
+++ 8-8 - API-COUNTRIES/scripts.js
@@ -8,17 +8,19 @@ function submits() {
   req.open("GET", `https://api.api-ninjas.com/v1/country?name=${city}`);
   req.setRequestHeader("X-Api-Key", apikey);
   req.send();
-
   document.querySelector(".flag").classList.remove("hidden");
   document.querySelector(".flag").style.transition = "2s";
   req.addEventListener("load", function () {
     const data = JSON.parse(req.responseText);
-
+    
+    const render = data[0].iso2;
+    document.querySelector(".flag").style.backgroundImage= `url('https://flagsapi.com/${render}/shiny/64.png')`;;
     const capital = data[0].capital;
     const cache = data[0].currency.name;
     const region = data[0].region;
     const torist = data[0].tourists;
     const a = data[0].population;
-    console.log(capital, region);
+    console.log(render);
+    console.log(render);
   });
 }
