72299e7d2c8772c2e661a1ca0f2303067ea089d9
WINFOORD <winford.op@gmail.com>
2024-08-08 16:57:00 +0330

    set error msg

--- 8-8 - API-COUNTRIES/scripts.js
+++ 8-8 - API-COUNTRIES/scripts.js
@@ -1,40 +1,46 @@
 "use strict";
-const username = document.querySelector(".username");
+const username = document.querySelector("#username");
 const apikey = "sadc5YaKUOoD27sZOtFjHg==jIAJwcSjZf3LG0WR";
 
 function submits() {
-
-
-
-  
-  document.querySelector(".region").classList.remove("hidden");
- document.querySelector(".cache").classList.remove("hidden");
-  document.querySelector(".capital").classList.remove("hidden");
-  document.querySelector(".torist").classList.remove("hidden");
-   document.querySelector(".pop").classList.remove("hidden");
-  
   const city = username.value;
+  document.querySelector(".region").classList.remove("hidden");
+  document.querySelector(".cache").classList.remove("hidden");
+  document.querySelector(".capital").classList.remove("hidden");
 
+  document.querySelector(".tourists").classList.remove("hidden");
 
-
-
-  const req = new XMLHttpRequest();
-  req.open("GET", `https://api.api-ninjas.com/v1/country?name=${city}`);
-  req.setRequestHeader("X-Api-Key", apikey);
-  req.send();
+  document.querySelector(".pop").classList.remove("hidden");
   document.querySelector(".flag").classList.remove("hidden");
   document.querySelector(".flag").style.transition = "2s";
-  req.addEventListener("load", function () {
-    const data = JSON.parse(req.responseText);
-    
-    const render = data[0].iso2;
-    document.querySelector(".flag").style.backgroundImage= `url('https://flagsapi.com/${render}/shiny/64.png')`;;
-    const capital = data[0].capital;
-    const cache = data[0].currency.name;
-    const region = data[0].region;
-    const torist = data[0].tourists;
-    const pop = data[0].population;
-    console.log(render);
- 
-  });
+
+  fetch(`https://api.api-ninjas.com/v1/country?name=${city}`, {
+    headers: { "X-Api-Key": apikey },
+  })
+    .then((abcv) => abcv.json())
+    .then((data) => {
+      const render = data[0].iso2;
+      document.querySelector(
+        ".flag"
+      ).style.backgroundImage = `url('https://flagsapi.com/${render}/shiny/64.png')`;
+      const capital = data[0].capital;
+      const cache = data[0].currency.name;
+      const region = data[0].region;
+      const tourists = data[0].tourists;
+      const pop = data[0].population;
+
+      document.querySelector(".region").value = "⛪region:  " + region;
+      document.querySelector(".cache").value = "💰Cache:  " + cache;
+      document.querySelector(".pop").value =
+        "🏠population:  " + pop / 1000 + "M";
+      document.querySelector(".tourists").value = "✈️tourists:  " + tourists;
+      document.querySelector(".capital").value = "🌆capital:  " + capital;
+
+      console.log(render);
+    })
+    .catch((error) => {
+      document.querySelector(".flag").style.backgroundColor = "black";
+      document.querySelector(".flag").textContent = "wrong!!";
+      console.log("Error:", error);
+    });
 }
