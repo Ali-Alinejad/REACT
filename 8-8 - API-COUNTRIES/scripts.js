a592a42769e3db4a251b8dbf641e1ccb921ea653
WINFOORD <winford.op@gmail.com>
2024-08-08 09:18:07 +0330

    +

--- 8-8 - API-COUNTRIES/scripts.js
+++ 8-8 - API-COUNTRIES/scripts.js
@@ -3,7 +3,21 @@ const username = document.querySelector(".username");
 const apikey = "sadc5YaKUOoD27sZOtFjHg==jIAJwcSjZf3LG0WR";
 
 function submits() {
+
+
+
+  
+  document.querySelector(".region").classList.remove("hidden");
+ document.querySelector(".cache").classList.remove("hidden");
+  document.querySelector(".capital").classList.remove("hidden");
+  document.querySelector(".torist").classList.remove("hidden");
+   document.querySelector(".pop").classList.remove("hidden");
+  
   const city = username.value;
+
+
+
+
   const req = new XMLHttpRequest();
   req.open("GET", `https://api.api-ninjas.com/v1/country?name=${city}`);
   req.setRequestHeader("X-Api-Key", apikey);
@@ -19,8 +33,8 @@ function submits() {
     const cache = data[0].currency.name;
     const region = data[0].region;
     const torist = data[0].tourists;
-    const a = data[0].population;
+    const pop = data[0].population;
     console.log(render);
-    console.log(render);
+ 
   });
 }
