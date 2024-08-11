242bdd81b5c6495c66ac6d7892a53367e7e3e772
WINFOORD <winford.op@gmail.com>
2024-08-11 18:26:14 +0330

    install parcel

---
+++ 11-8NPM/dist/main.5e469f4a.js
@@ -0,0 +1,10 @@
+"use strict";
+const sellmarket = function(jens, num) {
+    const barbari = 25.0;
+    const killo = 15;
+    const koll = num * killo;
+    return console.log(`tedad ${num} ta ${jens} ba majmo ${koll} hesab shod`);
+};
+sellmarket("sib", 5);
+
+//# sourceMappingURL=main.5e469f4a.js.map
