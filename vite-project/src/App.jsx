1368909d97da15159fd167a43ec0a2afaa4823e8
WINFOORD <winford.op@gmail.com>
2024-08-15 19:34:40 +0330

    sd

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,5 @@
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
+import bg from "../src/assets/config.png";
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
@@ -10,22 +10,37 @@ function Header() {
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[genRandomInt(reactDescriptions.length)]} React
+        concepts you will need for almost any app you are going to build!
       </p>
+      <h2>OPS Just For A Test</h2>
     </header>
   );
 }
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src="111"></img>
+      <h2>{props.Title}</h2>
+      <p>{props.matn}</p>
+    </li>
+  );
+}
 
-function App() {
+function RUN() {
   return (
     <div>
       <Header />
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcepts Title=" khar" matn=" khiardajsdnjkandjasd" />
+          </ul>
+        </section>
       </main>
     </div>
   );
 }
 
-export default App;
+export default RUN;
