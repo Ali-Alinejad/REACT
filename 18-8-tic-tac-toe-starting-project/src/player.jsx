f332064b3cc252cea8be1a57b1bc898722a916b4
WINFOORD <winford.op@gmail.com>
2024-08-18 21:06:37 +0330

    its ok enough for today  date : 18-8-2024

--- 18-8-tic-tac-toe-starting-project/src/player.jsx
+++ 18-8-tic-tac-toe-starting-project/src/player.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+
 export default function PlayersInfo({ name, symbol }) {
-  <li>
-    <span className="player">
-      <span className="player-name">{name}</span>
-      <span className="player-symbol">{symbol}</span>
-    </span>
-    <button>EDIT</button>
-  </li>;
+  const [Aa, Bb] = useState(false);
+  const [nowName, NewName] = useState(name);
+
+  const handleInputChange = (event) => {
+    NewName(event.target.value);
+  };
+
+  function changeReact() {
+    Bb((e) => !e);
+  }
+  let plyersNameShow = <span className="player-name">{nowName}</span>;
+  let plyersNameShow2 = (
+    <input type="text" required value={nowName} onChange={handleInputChange} />
+  );
+
+  if (Aa) {
+    plyersNameShow = plyersNameShow2;
+  }
+
+  return (
+    <li>
+      {plyersNameShow}
+      <span className="player">
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={changeReact}>{Aa ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
