3daa15f1b41d99dc3f8fb1f27c86c52603dc9e1a
WINFOORD <winford.op@gmail.com>
2024-08-18 10:42:09 +0330

    .

--- vite-tests/src/App.jsx
+++ vite-tests/src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  let [Aa, Bb] = useState(" ");
+  function clicked() {
+    {
+      Bb((prevColor) => (prevColor === "black" ? "red" : "black"));
+    }
+  }
+  return (
+    <div>
+      <section className="changeColor">
+        <h2 style={{ color: Aa }}>TEST COLOR</h2>
+        <button onClick={clicked}>CLICK</button>
+      </section>
+    </div>
+  );
 }
-
 export default App;
