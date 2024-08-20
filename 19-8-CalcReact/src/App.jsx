431f9fad25f51f92f309c7d16753652743256873
WINFOORD <winford.op@gmail.com>
2024-08-20 17:01:05 +0330

    set active class

--- 19-8-CalcReact/src/App.jsx
+++ 19-8-CalcReact/src/App.jsx
@@ -1,18 +1,34 @@
-const Pizza = [
-  { Name: "peperoni", Price: "150 T", key: "1" },
-  { Name: "Mix", Price: "180 T", key: "2" },
-  { Name: "special", Price: "200 T", key: "3" },
-];
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const Numbers = ["HI", "Bye", "hey"];
+  const [massage, setMassage] = useState(0);
+  function nextHandle() {
+    setMassage(massage + 1);
+    if (massage === Numbers.length - 1) {
+      return setMassage(Numbers.length - 1);
+    }
+  }
+
+  function BackHandle() {
+    setMassage(massage - 1);
+    if (massage === 0) {
+      return setMassage(0);
+    }
+  }
+
   return (
-    <div>
-      {Pizza.map((item) => (
-        <div key={item.key}>
-          <h1>{item.Name}</h1>
-          <h3>{item.Price}</h3>
-        </div>
-      ))}
+    <div id="section">
+      <div id="top">
+        <button onClick={BackHandle}>Back</button>
+        <label htmlFor=" ">{Numbers[massage]}</label>
+        <button onClick={nextHandle}>Next</button>
+      </div>
+      <div id="bot">
+        <label className={`${massage >= 0 ? "active" : ""}`}>1</label>
+        <label className={`${massage >= 1 ? "active" : ""}`}> 2</label>
+        <label className={`${massage >= 2 ? "active" : ""}`}>3</label>
+      </div>
     </div>
   );
 }
