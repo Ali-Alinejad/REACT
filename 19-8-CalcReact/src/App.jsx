9d3f04140f0708b2555b2f910dbc4824913629d9
WINFOORD <winford.op@gmail.com>
2024-08-20 15:20:23 +0330

    test map with jonas

--- 19-8-CalcReact/src/App.jsx
+++ 19-8-CalcReact/src/App.jsx
@@ -1,7 +1,20 @@
+const Pizza = [
+  { Name: "peperoni", Price: "150 T", key: "1" },
+  { Name: "Mix", Price: "180 T", key: "2" },
+  { Name: "special", Price: "200 T", key: "3" },
+];
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <div>
+      {Pizza.map((item) => (
+        <div key={item.key}>
+          <h1>{item.Name}</h1>
+          <h3>{item.Price}</h3>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
