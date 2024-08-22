314f0418d87e24341d3c896f33a336bf1f3eae3e
WINFOORD <winford.op@gmail.com>
2024-08-22 20:18:10 +0330

    set

--- 22-8-Qabox/src/App.jsx
+++ 22-8-Qabox/src/App.jsx
@@ -1,10 +1,57 @@
+import { useState } from "react";
 import "./App.css";
+
+const QA = [
+  {
+    title: "who we worked ?",
+    Answer: "well we try to catch some data from us object ",
+  },
+  {
+    title: "who we Jug ?",
+    Answer: "Lorem ipsum dolor sit amet consectetur adipisicing e",
+  },
+  {
+    title: "who we Jug ?",
+    Answer: "Lorem ipsum dolor sit amet consectetur adipisicing e",
+  },
+];
 
 function App() {
   return (
     <>
-      <div></div>
+      <Rendering Data={QA} />
     </>
+  );
+}
+
+function Rendering({ Data }) {
+  return (
+    <div className="acc">
+      {Data.map((e, index) => (
+        <SStyles
+          title={e.title}
+          content={e.Answer}
+          key={index}
+          number={index + 1}
+        />
+      ))}
+    </div>
+  );
+}
+
+function SStyles({ title, content, number }) {
+  const [onShow, setonShow] = useState(false);
+
+  const handleClick = () => {
+    setonShow((prev) => !prev);
+  };
+
+  return (
+    <div className="main"  onClick={handleClick}>
+      <h1>{number}</h1>
+      <h1>{title}</h1>
+      <p className="content-box">{onShow ? content : ""}</p>
+    </div>
   );
 }
 
