018d45cd3dd6956605355f58a53d8d756c110edc
WINFOORD <winford.op@gmail.com>
2024-09-01 01:17:31 +0330

    finaly i did it in 2 am

--- 31-8-reduceTest/src/App.jsx
+++ 31-8-reduceTest/src/App.jsx
@@ -1,5 +1,6 @@
-import Bank from "./Bank.jsx";
+/* eslint-disable no-unused-vars */
 import { useReducer } from "react";
+import "./Bank.css"; // Import your CSS file
 
 function App() {
   const initialState = {
@@ -7,36 +8,72 @@ function App() {
     loan: 0,
     isActive: false,
   };
-  function reduce(state,action) {
-  
+  function Reducer(state, action) {
     switch (action.type) {
       case "openAccount":
-        return ''
+        console.log(state);
+        return {
+          ...state,
+          balance: 500,
+          isActive: true,
+        };
       case "withdraw":
-        return ''
-      case "deposit":
-        return ''
-      case "CloseAccount":
-        return ''
-      case "ReqLoan":
-        return ''
-      case "PayLoan":
-        return ''
-      
+        return {
+          ...state,
+          balance: 500, // Assuming a fixed withdrawal amount
+        };
 
+      case "deposit":
+        return "";
+      case "CloseAccount":
+        return "";
+      case "ReqLoan":
+        return "";
+      case "PayLoan":
+        return "";
+      default:
+        throw new Error("unknown action type " + action.type);
     }
-}
+  }
   const [{ balance, loan, isActive }, dispatch] = useReducer(
-    reduce,
+    Reducer,
     initialState
   );
   return (
     <>
-      <Bank
-        balance={initialState.balance}
-        isActive={initialState.isActive}
-        loan={initialState.loan}
-      />
+      <div className="bank-container">
+        <h1 className="bank-title">use Reducer Bank Acc</h1>
+        <p className="balance">Balance: {balance}</p>
+        <p className="loan">Loan: {loan}</p>
+
+        <button
+          className="bank-button"
+          onClick={() => dispatch({ type: "openAccount" })}
+          disabled={isActive}
+        >
+          Open Account
+        </button>
+
+        <button className="bank-button" disabled={!isActive}>
+          Deposit $150
+        </button>
+        <button
+          className="bank-button"
+          disabled={!isActive}
+          onClick={() => dispatch({ type: "withdraw" })}
+        >
+          Withdraw $50
+        </button>
+        <button className="bank-button" disabled={!isActive}>
+          Request a Loan of $5000
+        </button>
+        <button className="bank-button" disabled={!isActive}>
+          Pay Loan
+        </button>
+        <button className="bank-button" disabled={!isActive}>
+          Close Account
+        </button>
+      </div>
     </>
   );
 }
