28b62641ae1fb0f3519889917b69e1d79cfaf6d2
WINFOORD <winford.op@gmail.com>
2024-09-01 15:12:56 +0330

    new project

--- vite-project/components/menubot.jsx
+++
@@ -1,9 +0,0 @@
-import "./MenuBot.css";
-
-export default function Btn1({ children, selected, isSelect }) {
-  return (
-    <button className={isSelect ? "active" : undefined} onClick={selected}>
-      {children}
-    </button>
-  );
-}
